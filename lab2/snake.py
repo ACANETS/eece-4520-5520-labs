fa10eeee287ef6fb717bb6e570611c8f5539d915
Yan Luo <yan_luo@uml.edu>
2020-10-21 20:11:16 -0400

    add TODO comments for init serial port

--- lab2/snake.py
+++ lab2/snake.py
@@ -8,11 +8,14 @@
 import turtle
 import time
 import random
+# TODO uncomment the following line to use pyserial package
+#import serial
 
 # Note the serial port dev file name
 # need to change based on the particular host machine
-serialDevFile = '/dev/cu.usbmodem14201'
-ser=serial.Serial(serialDevFile, 9600, timeout=0)
+# TODO uncomment the following two lines to initialize serial port
+#serialDevFile = '/dev/cu.usbmodem14201'
+#ser=serial.Serial(serialDevFile, 9600, timeout=0)
 
 delay = 0.1
 
